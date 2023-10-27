ff7d7cb0236224b96b1db8bdd870a68323f5adb6
Reza <reza.amini.b@gmail.com>
2023-10-28 00:20:26 +0100

    - retrieved all of the attribute values and... added the script tag

--- script/script.js
+++ script/script.js
@@ -1,1 +1,7 @@
-console.log("ssasdasd");
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const deleteButton = document.querySelector("[data-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandElement = document.querySelector("[data-previous-operand]");
+const currentOperandElement = document.querySelector("[data-current-operand]");
