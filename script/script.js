39fffbe28cd37f70f129c5dec0983600ef467416
Reza <reza.amini.b@gmail.com>
2023-10-28 00:23:16 +0100

    - added a new calculator class in the... sctript file with a constructor and blank methods.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,30 @@
+
+class Calculator {
+    constructor(previousOperandElement, currentOperandElement){
+        this.previousOperandElement = previousOperandElement;
+        this.currentOperandElement = currentOperandElement;
+    }
+
+    clear() {
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number){
+    }
+
+    chooseOperation(operation){
+    }
+
+    compute() {
+    }
+
+    updateDisplay() {
+    }
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
@@ -5,3 +32,5 @@ const deleteButton = document.querySelector("[data-delete]");
 const allClearButton = document.querySelector("[data-all-clear]");
 const previousOperandElement = document.querySelector("[data-previous-operand]");
 const currentOperandElement = document.querySelector("[data-current-operand]");
+
+const calculator = new Calculator(previousOperandElement, currentOperandElement);
