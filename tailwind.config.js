c5e103198983b7a9f53c0454b91690f52d85eec0
Reza <reza.amini.b@gmail.com>
2023-11-02 14:16:16 +0000

    - fixed the output row height to expand based... on the operand numbers.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // added new 4 column grid as new4
+        'new4': 'repeat(4, minmax(100px,100px))',
+        }
+    },
   },
   variants: {
     extend: {},
